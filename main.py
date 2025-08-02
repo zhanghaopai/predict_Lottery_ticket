5e81327e2db0329dd448b9163db32339aa69d50c
zhang_haopai <zhang_haopai@venusgroup.com.cn>
2025-08-02 15:29:34 +0800

    feat(main): 添加模型预测功能- 导入 run_predict 模块 - 在主函数中添加模型预测的执行语句

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 @Date    ：2025/8/2 14:58 
 '''
 import get_data
+import run_predict
 import run_train_model
 
 if __name__ == "__main__":
@@ -14,4 +15,5 @@ if __name__ == "__main__":
     # 获取数据
     get_data.run(name=name)
     run_train_model.run(name=name, train_test_split=0.8)
+    run_predict.run(name=name)
     exit(0)
