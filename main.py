ead20585e49dd02393100033b5aa0f0193dbd30e
zhang_haopai <zhang_haopai@venusgroup.com.cn>
2025-08-02 15:24:50 +0800

    feat: 添加主程序入口文件- 新增 main.py 文件作为项目入口 - 实现数据获取和模型训练的主要逻辑 - 设置双色球 lottery 类型 -指定训练集和测试集划分比例为 0.8

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''
+@Project ：predict_Lottery_ticket 
+@File    ：main
+@Author  ：zhanghaopai@outlook.com
+@Date    ：2025/8/2 14:58 
+'''
+import get_data
+import run_train_model
+
+if __name__ == "__main__":
+    name = "ssq"
+    # 获取数据
+    get_data.run(name=name)
+    run_train_model.run(name=name, train_test_split=0.8)
+    exit(0)
